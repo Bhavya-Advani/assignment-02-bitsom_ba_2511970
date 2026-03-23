3106a337679d42ff553ce819a457ea229c6ddee6
Bhavya-Advani <advanibhavya26@gmail.com>
2026-03-23 10:39:27 +0530

    Update mongo_queries.js

--- part2-nosql/mongo_queries.js
+++ part2-nosql/mongo_queries.js
@@ -1,1 +1,47 @@
-
+[
+  {
+    "_id": "E101",
+    "name": "Samsung 55 inch Smart TV",
+    "category": "Electronics",
+    "price": 55000,
+    "brand": "Samsung",
+    "warranty_years": 2,
+    "specifications": {
+      "resolution": "4K",
+      "voltage": "220V",
+      "screen_type": "LED"
+    },
+    "features": ["Smart TV", "WiFi", "Bluetooth"],
+    "stock": 25
+  },
+  {
+    "_id": "C101",
+    "name": "Men's Denim Jacket",
+    "category": "Clothing",
+    "price": 2500,
+    "brand": "Levis",
+    "size_options": ["S", "M", "L", "XL"],
+    "material": "Denim",
+    "colors": ["Blue", "Black"],
+    "ratings": {
+      "average": 4.5,
+      "reviews": 120
+    },
+    "stock": 50
+  },
+  {
+    "_id": "G101",
+    "name": "Organic Milk 1L",
+    "category": "Groceries",
+    "price": 60,
+    "brand": "Amul",
+    "expiry_date": "2024-12-20",
+    "nutritional_info": {
+      "calories": 150,
+      "protein": "8g",
+      "fat": "7g"
+    },
+    "storage": "Refrigerated",
+    "stock": 200
+  }
+]
